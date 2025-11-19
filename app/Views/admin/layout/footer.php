4c53bc4b166ee26ac1a2c4e24918f36ddcd68b75
Pratama Putra <theperfectpratama.stu@pnc.ac.id>
2025-11-19 13:30:42 +0700

    capek bikin commit terus

--- app/Views/admin/layout/footer.php
+++ app/Views/admin/layout/footer.php
@@ -11,7 +11,7 @@
   });
 </script>
 
-<div id="custom-confirm-modal" class="relative z-[60] hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div id="custom-confirm-modal" class="relative z-60 hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   
   <div id="modal-backdrop" class="fixed inset-0 bg-gray-900/75 transition-opacity duration-300 opacity-0"></div>
 
@@ -22,7 +22,7 @@
         
         <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
-            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+            <div class="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
               <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
               </svg>
