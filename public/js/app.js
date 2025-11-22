003d4043c517d8ca37806d1776c3a03fea5b0c5b
Pratama Putra <theperfectpratama.stu@pnc.ac.id>
2025-11-22 10:26:48 +0700

    kommit terbaru sygg

--- public/js/app.js
+++ public/js/app.js
@@ -78,21 +78,6 @@ function calculateTotal() {
     }
 }
 
-
-
-/**
- * Fungsi: Hitung Kontras Warna (Hitam/Putih)
- */
-function getContrastColor(hex) {
-    if (!hex || hex.length < 7) return 'black';
-    hex = hex.substring(1);
-    const r = parseInt(hex.substring(0, 2), 16);
-    const g = parseInt(hex.substring(2, 4), 16);
-    const b = parseInt(hex.substring(4, 6), 16);
-    const luminance = (0.299 * r + 0.587 * g + 0.114 * b);
-    return (luminance > 145) ? 'black' : 'white'; 
-}
-
 /**
  * Fungsi: Hapus Event (Admin)
  */
